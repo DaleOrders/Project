8f2df6bdd787d5cfdc0bf9a6e0b7e64a2e9fb9a6
DaleOrders <52617475+DaleOrders@users.noreply.github.com>
2020-06-01 14:01:59 +1000

    Update particle_photon_motion.ino

--- particle_photon_motion.ino
+++ particle_photon_motion.ino
@@ -4,19 +4,21 @@ int led1 = D4;
 void setup ()
 {
     pinMode(led1,OUTPUT);
-    
+    //wait for 10 minutes before 
     Particle.subscribe("motiondetected", handler);
-    delay(3000);
+    delay(600000);
 }
 
 void handler(const char *event, const char *data) {
     
+    
     if(strcmp(event, "motiondetected")==0){
       digitalWrite(led1,HIGH);
-      delay(200);
-      digitalWrite(led1,LOW);
       
-        
+     else{
+            digitalWrite(led1,LOW);
+     }
+            
     }
 
 }
